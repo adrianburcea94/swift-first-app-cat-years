cdd6e7e06c04ae1147368c3b1ab08319a176efda
Adrian Burcea <adrianburcea@192-168-0-112.rdsnet.ro>
2018-03-01 19:50:46 +0200

    Implement functionality

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelCatAge: UITextField!
+    @IBOutlet weak var labelCatYears: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToAgeYears(_ sender: UIButton) {
+        
+        if let age = labelCatAge.text {
+            if let ageAsNumber = Int(age) {
+                let ageIntCatYears = ageAsNumber * 7;
+                
+                // Show the result
+                let output = "\(ageIntCatYears)";
+                labelCatYears.text = "Your cat has " + output + " in cat years";
+            }
+        }
 
+
+    }
+    
 }
 
